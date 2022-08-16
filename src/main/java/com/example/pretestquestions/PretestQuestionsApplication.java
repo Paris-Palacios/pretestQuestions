5be91b2e2e45b80684c3c7194cdef04b3f95e68c
Paris Palacios <paris.palacios@netlogistik.com>
2022-08-16 07:11:09 -0500

    initial commit

---
+++ src/main/java/com/example/pretestquestions/PretestQuestionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.pretestquestions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PretestQuestionsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PretestQuestionsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pretestquestions/PretestQuestionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pretestquestions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PretestQuestionsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
